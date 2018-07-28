Fix off-by-one palette index problem:
The code was adding a black (0, 0, 0) palette entry in front of all the quantised colour entries when in fact this shouldn’t be done.

This means that in the resulting Vox model, everything that should have the first (index 0) palette colour actually has the black colour, then everything that should have the 2nd (index 1) palette colour has the 1st palette colour, and so on.

Note: The spec for the Vox format says that the indexes of your palette entries end up getting +1’d, due to the ‘empty’ reserved value, but doesn’t say that you should put any kind of dummy value at index 0.

pyvox/models.py
@@ -71,7 +71,7 @@ class Vox(object):
             # color index 0 is reserved for empty, so we get 255 colors
             img = Image.fromarray(a.reshape(x,y*z,3)).quantize(255)
             palette = img.getpalette()
-            palette = [ Color(0,0,0,0) ] + [ Color(*c, 255) for c in chunks(palette, 3) ]
+            palette = [ Color(*c, 255) for c in chunks(palette, 3) ]
             a = np.asarray(img, dtype='B').reshape(x,y,z).copy() + 1
             a[mask] = 0
 